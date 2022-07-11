ba657a7cb5cb56e76fd2f7b0719d8d34ad17dbba
arafat-alim <arafat97alim@gmail.com>
2022-07-11 19:57:10 +0530

    added more functionality.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Showdown from "showdown";
 import ReactMde from "react-mde";
 
-export default function Editor() {
+export default function Editor(props) {
+  console.log(props);
+  //! create a state for selected tab
+  const [selectedTab, setSelectedTab] = useState("Write");
   const converter = new Showdown.Converter({
     tables: true,
     simplifiedAutoLink: true,
@@ -12,6 +15,10 @@ export default function Editor() {
   return (
     <section className="pane editor">
       <ReactMde
+        value={props.currentNote.body}
+        onTabChange={setSelectedTab}
+        onChange={props.updateNote}
+        selectedTab={selectedTab}
         minEditorHeight={80}
         heightUnits="vh"
         generateMarkdownPreview={(markdown) =>
