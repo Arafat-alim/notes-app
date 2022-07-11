3cc554309153373c3b8794583345509b9880ed94
arafat-alim <arafat97alim@gmail.com>
2022-07-11 14:41:22 +0530

    basic component boilerplates added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import Split from "react-split";
+import Editor from "./components/Editor";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Split>
+        <Sidebar />
+        <Editor />
+      </Split>
+    </div>
+  );
 }
 
 export default App;
