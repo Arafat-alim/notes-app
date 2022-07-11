256f8ce0d4ee9a8eb9635c5ca047c2963ed3ef48
arafat-alim <arafat97alim@gmail.com>
2022-07-11 19:14:25 +0530

    finally showdown is working perfectily.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Showdown from "showdown";
+import ReactMde from "react-mde";
 
 export default function Editor() {
+  const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+  });
   return (
-    <div>
-      <h1>Editor</h1>
-    </div>
+    <section className="pane editor">
+      <ReactMde
+        minEditorHeight={80}
+        heightUnits="vh"
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+      />
+    </section>
   );
 }
